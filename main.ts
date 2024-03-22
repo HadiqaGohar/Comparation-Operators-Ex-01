e58d4abe3023789c98b96884ce5dc0decfd352cc
HadiqaGohar <160311280+HadiqaGohar@users.noreply.github.com>
2024-03-22 08:12:08 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,11 @@
+//COMPARATION OPERATORS
+// CAKES
+let cake = "Chocolate Cake"
+if (cake === "Cup Cake") {
+    console.log ("I really like chocolate cake")
+} else if (cake === "Chocolate Cake") {
+    console.log ("I like Chocolate Cake")
+} else if (cake === "Strewberry Cake") {
+    console.log ("I dont like Strewberry Cake")
+} else 
+    console.log ("I don't like cake")
